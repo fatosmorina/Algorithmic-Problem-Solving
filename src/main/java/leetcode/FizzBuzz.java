74fcc6fdc8e81ce7c4618296b3584070ccfbc4d9
Fatos Morina <fatosi.morina@gmail.com>
2017-01-27 20:03:28 +0100

    Solve FizzBuzz

---
+++ src/main/java/leetcode/FizzBuzz.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+	List<String> list = new ArrayList<String>();
+	for (int i = 1; i < n + 1; i++) {
+	    if (i % 3 == 0 && i % 5 == 0) {
+		list.add("FizzBuzz");
+	    } else if (i % 3 == 0) {
+		list.add("Fizz");
+	    } else if (i % 5 == 0) {
+		list.add("Buzz");
+	    } else {
+		list.add(i + "");
+	    }
+	}
+	return list;
+    }
+
+}
