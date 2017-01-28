90b82c6cd1b88367a5c287f83ec4b387d12eb0c7
Fatos Morina <fatosi.morina@gmail.com>
2017-01-28 08:04:18 +0100

    Solve LongestCommonPrefix

---
+++ src/main/java/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strings) {
+	int numberOfElements = strings.length;
+	if (numberOfElements == 0) {
+	    return "";
+	} else if (numberOfElements == 1) {
+	    return strings[0];
+	}
+	int minLength = strings[0].length();
+	for (String string : strings) {
+	    if (minLength > string.length()) {
+		minLength = string.length();
+	    }
+	}
+	int lastIndex = minLength;
+	String prefix = strings[0].substring(0, lastIndex);
+	for (int i = 0; i < numberOfElements; i++) {
+	    if (!strings[i].substring(0, lastIndex).contains(prefix)) {
+		i = -1;
+		lastIndex = lastIndex - 1;
+		prefix = strings[0].substring(0, lastIndex);
+	    }
+	}
+	return prefix;
+    }
+
+}
