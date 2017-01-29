9053156e78fa4d5d14c4ea988e50afc29569892e
Fatos Morina <fatosi.morina@gmail.com>
2017-01-29 10:28:19 +0100

    Solve Count Primes

---
+++ src/main/java/leetcode/CountPrimes.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package leetcode;
+
+/**
+ * FatosMorina.com
+ */
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+	int counter = 0;
+	boolean[] isPrime = generatePrimes(n);
+	for (int i = 0; i < isPrime.length; i++) {
+	    if (isPrime[i]) {
+		counter++;
+	    }
+	}
+	return counter;
+    }
+
+    public static boolean[] generatePrimes(int number) {
+	boolean[] isPrime = new boolean[number];
+	for (int i = 2; i < number; i++) {
+	    isPrime[i] = true;
+	}
+	for (int i = 2; i * i < number; i++) {
+	    if (isPrime[i]) {
+		for (int j = i; j * i < number; j++) {
+		    isPrime[i * j] = false;
+		}
+	    }
+	}
+	return isPrime;
+    }
+}
