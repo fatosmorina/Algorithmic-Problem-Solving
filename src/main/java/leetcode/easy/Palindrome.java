cb4048f116dac0d9d9295196dfacda0a3da9900a
Fatos Morina <fatosi.morina@gmail.com>
2017-01-31 21:10:39 +0100

    Add problem URL

--- src/main/java/leetcode/easy/Palindrome.java
+++ src/main/java/leetcode/easy/Palindrome.java
@@ -6,6 +6,7 @@ package leetcode.easy;
 /**
  * Fatos Morina
  */
+// https://leetcode.com/problems/palindrome-number/
 public class Palindrome {
     public boolean isPalindrome(int x) {
 	return new StringBuilder(x + "").reverse().toString().equals(x + "");
