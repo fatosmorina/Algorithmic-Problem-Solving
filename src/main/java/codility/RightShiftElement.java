15ee842f0e913a6df9d6ac4ce48f2acde180ad20
Fatos Morina <fatosi.morina@gmail.com>
2017-02-12 07:23:55 +0100

    Refactor RightShiftElement

--- src/main/java/codility/RightShiftElement.java
+++ src/main/java/codility/RightShiftElement.java
@@ -3,9 +3,10 @@ package codility;
 public class RightShiftElement {
 
     public int[] solution(int[] array, int k) {
-	int[] shifted = new int[array.length];
-	for (int i = 0; i < array.length; i++) {
-	    shifted[(k + i) % array.length] = array[i];
+	int length = array.length;
+	int[] shifted = new int[length];
+	for (int i = 0; i < length; i++) {
+	    shifted[(k + i) % length] = array[i];
 	}
 	return shifted;
     }
