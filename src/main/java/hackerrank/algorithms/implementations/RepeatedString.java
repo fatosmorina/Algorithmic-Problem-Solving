99d40262e33f8f0a533ea8ab4d349af4b04a4087
Fatos Morina <fatosi.morina@gmail.com>
2017-02-19 14:44:02 +0100

    Solve RepeatedString

--- src/main/java/hackerrank/algorithms/implementations/RepeatedString.java
+++ src/main/java/hackerrank/algorithms/implementations/RepeatedString.java
@@ -1,22 +1,52 @@
 package hackerrank.algorithms.implementations;
 
 import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
 import static java.lang.System.exit;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
+//https://www.hackerrank.com/challenges/repeated-string
 public class RepeatedString {
     static void solve() throws Exception {
+	Scanner input = new Scanner(System.in);
+	System.out.println(getNthLetter(next(), nextLong()));
+	input.close();
+    }
 
+    public static long getNthLetter(String input, long index) {
+	if (!input.contains("a")) {
+	    return 0;
+	}
+	int length = input.length();
+	long ratio = index / length;
+	long remainder = index % length;
+	long counter = countALetter(input, length) * ratio + countALetter(input, remainder);
+	return counter;
+    }
+
+    public static long countALetter(String input, long upperLimit) {
+	long counter = 0;
+	for (int i = 0; i < upperLimit; i++) {
+	    if (input.charAt(i) == 'a') {
+		counter++;
+	    }
+	}
+	return counter;
     }
 
     static int nextInt() throws IOException {
 	return parseInt(next());
+    }
+
+    static long nextLong() throws IOException {
+	return parseLong(next());
     }
 
     static String next() throws IOException {
--- src/test/java/hackerrank/algorithms/implementations/RepeatedStringTest.java
+++ src/test/java/hackerrank/algorithms/implementations/RepeatedStringTest.java
@@ -1,45 +1,26 @@
 package hackerrank.algorithms.implementations;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("static-access")
 public class RepeatedStringTest {
-    static void solve() throws Exception {
 
+    @InjectMocks
+    RepeatedString repeatedString;
+
+    @Test
+    public void shouldReturnSeven() {
+	assertEquals(7, repeatedString.getNthLetter("aba", 10));
     }
 
-    static int nextInt() throws IOException {
-	return parseInt(next());
+    @Test
+    public void shouldReturnLargeNumber() {
+	assertEquals(1000000000000l, repeatedString.getNthLetter("a", 1000000000000l));
     }
-
-    static String next() throws IOException {
-	while (tok == null || !tok.hasMoreTokens()) {
-	    tok = new StringTokenizer(in.readLine());
-	}
-	return tok.nextToken();
-    }
-
-    public static void main(String[] args) {
-	try {
-	    in = new BufferedReader(new InputStreamReader(System.in));
-	    out = new PrintWriter(new OutputStreamWriter(System.out));
-	    solve();
-	    in.close();
-	    out.close();
-	} catch (Throwable e) {
-	    e.printStackTrace();
-	    exit(0);
-	}
-    }
-
-    static BufferedReader in;
-    static PrintWriter out;
-    static StringTokenizer tok;
 }
