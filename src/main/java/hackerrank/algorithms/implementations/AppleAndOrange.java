37854737c0a5e5ee8489f904089f60f57aedd4e7
Fatos Morina <fatosi.morina@gmail.com>
2017-04-05 21:50:39 +0200

    Format code

--- src/main/java/hackerrank/algorithms/implementations/AppleAndOrange.java
+++ src/main/java/hackerrank/algorithms/implementations/AppleAndOrange.java
@@ -14,66 +14,66 @@ import java.util.StringTokenizer;
 
 public class AppleAndOrange {
 
-    static void solve() throws Exception {
-	int firstCoordinateOfHouse = nextInt();
-	int secondCoordinateOfHouse = nextInt();
-	int appleCenter = nextInt();
-	int orangeCenter = nextInt();
-	int numberOfApples = nextInt();
-	int numberOfOranges = nextInt();
-	int appleCounter = 0;
-	int orangeCounter = 0;
+	static void solve() throws Exception {
+		int firstCoordinateOfHouse = nextInt();
+		int secondCoordinateOfHouse = nextInt();
+		int appleCenter = nextInt();
+		int orangeCenter = nextInt();
+		int numberOfApples = nextInt();
+		int numberOfOranges = nextInt();
+		int appleCounter = 0;
+		int orangeCounter = 0;
 
-	for (int i = 0; i < numberOfApples; i++) {
-	    int currentDistance = nextInt();
-	    if (currentDistance < 0) {
-		continue;
-	    }
-	    int distance = appleCenter + currentDistance;
-	    if (distance >= firstCoordinateOfHouse && distance <= secondCoordinateOfHouse) {
-		appleCounter++;
-	    }
+		for (int i = 0; i < numberOfApples; i++) {
+			int currentDistance = nextInt();
+			if (currentDistance < 0) {
+				continue;
+			}
+			int distance = appleCenter + currentDistance;
+			if (distance >= firstCoordinateOfHouse && distance <= secondCoordinateOfHouse) {
+				appleCounter++;
+			}
+		}
+
+		for (int i = 0; i < numberOfOranges; i++) {
+			int currentDistance = nextInt();
+			if (currentDistance > 0) {
+				continue;
+			}
+			int distance = orangeCenter + currentDistance;
+			if (distance >= firstCoordinateOfHouse && distance <= secondCoordinateOfHouse) {
+				orangeCounter++;
+			}
+		}
+		System.out.println(appleCounter);
+		System.out.println(orangeCounter);
 	}
 
-	for (int i = 0; i < numberOfOranges; i++) {
-	    int currentDistance = nextInt();
-	    if (currentDistance > 0) {
-		continue;
-	    }
-	    int distance = orangeCenter + currentDistance;
-	    if (distance >= firstCoordinateOfHouse && distance <= secondCoordinateOfHouse) {
-		orangeCounter++;
-	    }
+	static int nextInt() throws IOException {
+		return parseInt(next());
 	}
-	System.out.println(appleCounter);
-	System.out.println(orangeCounter);
-    }
 
-    static int nextInt() throws IOException {
-	return parseInt(next());
-    }
-
-    static String next() throws IOException {
-	while (tok == null || !tok.hasMoreTokens()) {
-	    tok = new StringTokenizer(in.readLine());
+	static String next() throws IOException {
+		while (tok == null || !tok.hasMoreTokens()) {
+			tok = new StringTokenizer(in.readLine());
+		}
+		return tok.nextToken();
 	}
-	return tok.nextToken();
-    }
 
-    public static void main(String[] args) {
-	try {
-		in = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(new OutputStreamWriter(System.out));
-        solve();
-        in.close();
-        out.close();
-	} catch (Throwable e) {
-	    e.printStackTrace();
-	    exit(0);
+	public static void main(String[] args) {
+		try {
+			in = new BufferedReader(new InputStreamReader(System.in));
+			out = new PrintWriter(new OutputStreamWriter(System.out));
+			solve();
+			in.close();
+			out.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			exit(0);
+		}
 	}
-    }
 
-    static BufferedReader in;
-    static PrintWriter out;
-    static StringTokenizer tok;
+	static BufferedReader in;
+	static PrintWriter out;
+	static StringTokenizer tok;
 }
