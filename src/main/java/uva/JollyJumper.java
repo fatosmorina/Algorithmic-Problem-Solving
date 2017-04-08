5f7d7d54636c606339ca9d31364497deed93bc8f
Fatos Morina <fatosi.morina@gmail.com>
2017-04-08 20:25:54 +0200

    Remove unused method at JollyJumper

--- src/main/java/uva/JollyJumper.java
+++ src/main/java/uva/JollyJumper.java
@@ -1,19 +1,9 @@
 package uva;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.StringTokenizer;
 
-//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=submit_problem&problemid=979&category=
 public class JollyJumper {
 
 	public static void main(String[] args) {
@@ -37,16 +27,6 @@ public class JollyJumper {
 				System.out.println("Not jolly");
 			}
 		}
-	}
-
-	public static int findMaximalElement(int[] numbers) {
-		int max = numbers[0];
-		for (int i = 1; i < numbers.length; i++) {
-			if (max < numbers[i]) {
-				max = numbers[i];
-			}
-		}
-		return max;
 	}
 
 }
