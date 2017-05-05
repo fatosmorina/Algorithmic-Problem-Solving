89ff0f66f755ca48362e0094891b47a6a08afa36
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 11:43:22 +0200

    Refactor PseudoPrimeNumbers

--- src/main/java/uva/PseudoPrimeNumbers.java
+++ src/main/java/uva/PseudoPrimeNumbers.java
@@ -1,22 +1,15 @@
 package uva;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class PseudoPrimeNumbers {
 
-	static void solve() throws Exception {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		while (true) {
-			int p = nextInt();
-			int a = nextInt();
+			int p = input.nextInt();
+			int a = input.nextInt();
 			if (a == 0 && p == 0) {
 				break;
 			}
@@ -37,32 +30,4 @@ public class PseudoPrimeNumbers {
 			System.out.println(answer);
 		}
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
