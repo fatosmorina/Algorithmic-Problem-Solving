3227ef0744dfe4b9d2b969784cdcb54cf790e18f
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 11:20:35 +0200

    Add problem source URL for HighPrecisionNumber

--- src/main/java/uva/HighPrecisionNumber.java
+++ src/main/java/uva/HighPrecisionNumber.java
@@ -1,5 +1,7 @@
 package uva;
 
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2921
+
 import static java.lang.Integer.parseInt;
 import static java.lang.System.exit;
 
