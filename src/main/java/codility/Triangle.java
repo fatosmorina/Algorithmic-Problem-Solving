130e9fbc6d833a84db9a1a1c178327977a1682cd
Fatos Morina <fatosi.morina@gmail.com>
2017-05-05 08:43:34 +0200

    Solve LightMoreLight

--- src/main/java/codility/Triangle.java
+++ src/main/java/codility/Triangle.java
@@ -3,13 +3,13 @@ package codility;
 import java.util.Arrays;
 
 public class Triangle {
-    public int solution(int[] array) {
-	Arrays.sort(array);
-	for (int i = 0; i < array.length - 2; i++) {
-	    if ((long) array[i] + (long) array[i + 1] > (long) array[i + 2]) {
-		return 1;
-	    }
+	public int solution(int[] array) {
+		Arrays.sort(array);
+		for (int i = 0; i < array.length - 2; i++) {
+			if ((long) array[i] + (long) array[i + 1] > (long) array[i + 2]) {
+				return 1;
+			}
+		}
+		return 0;
 	}
-	return 0;
-    }
 }
---
+++ src/main/java/uva/LightMoreLight.java
@@ -0,0 +1,24 @@
+package uva;
+import java.util.Scanner;
+
+public class LightMoreLight {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		long number = input.nextLong();
+		while (number != 0) {
+			if (isAPerfectSquare(number)) {
+				System.out.println("yes");
+			} else {
+				System.out.println("no");
+			}
+			number = input.nextLong();
+		}
+	}
+
+	private static boolean isAPerfectSquare(long number) {
+		long squareRoot = (long) Math.sqrt(number);
+		return squareRoot * squareRoot == number;
+	}
+
+}
