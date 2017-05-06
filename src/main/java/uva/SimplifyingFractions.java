9ef565a91ed588d68f6507b675bb0511b3649c5a
Fatos Morina <fatosi.morina@gmail.com>
2017-05-06 06:07:11 +0200

    Refactor SimplifyingFractions

--- src/main/java/uva/SimplifyingFractions.java
+++ src/main/java/uva/SimplifyingFractions.java
@@ -22,19 +22,12 @@
 
 package uva;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class SimplifyingFractions {
 
-	static void solve() throws Exception {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int numberOfTestCases = input.nextInt();
 		while (numberOfTestCases != 0) {
@@ -53,28 +46,4 @@ public class SimplifyingFractions {
 		}
 		input.close();
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			solve();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
