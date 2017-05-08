b159a465cfc7797e0d0eb19346a4a8171c243b35
Fatos Morina <fatosi.morina@gmail.com>
2017-05-08 06:50:00 +0200

    Refactor DigitCounting

--- src/main/java/uva/DigitCounting.java
+++ src/main/java/uva/DigitCounting.java
@@ -1,5 +1,3 @@
-package uva;
-
 /**
  * Trung is bored with his mathematics homeworks. He takes a piece of chalk and starts writing a sequence
  * of consecutive integers starting with 1 to N (1 < N < 10000). After that, he counts the number of
@@ -22,25 +20,18 @@ package uva;
  * 0 1 1 1 0 0 0 0 0 0
  * 1 6 2 2 1 1 1 1 1 1
  */
+
 //https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3666
-
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3666
 
 public class DigitCounting {
 
-	static void solve() throws Exception {
-		int numberOfTestCases = nextInt();
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int numberOfTestCases = input.nextInt();
 		while (numberOfTestCases != 0) {
 			int[] numbers = new int[10];
-			int number = nextInt();
+			int number = input.nextInt();
 			for (int i = number; i > 0; i--) {
 				int j = i;
 				while (j != 0) {
@@ -58,32 +49,4 @@ public class DigitCounting {
 			numberOfTestCases--;
 		}
 	}
-
-	static int nextInt() throws IOException {
-		return parseInt(next());
-	}
-
-	static String next() throws IOException {
-		while (tok == null || !tok.hasMoreTokens()) {
-			tok = new StringTokenizer(in.readLine());
-		}
-		return tok.nextToken();
-	}
-
-	public static void main(String[] args) {
-		try {
-			in = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(new OutputStreamWriter(System.out));
-			solve();
-			in.close();
-			out.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-			exit(0);
-		}
-	}
-
-	static BufferedReader in;
-	static PrintWriter out;
-	static StringTokenizer tok;
 }
