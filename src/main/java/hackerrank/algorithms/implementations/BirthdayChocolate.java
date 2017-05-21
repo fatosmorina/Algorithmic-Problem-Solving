b0bed77d027166ea3d57b5a6967b1d3224ef4121
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 18:23:23 +0200

    Add problem description as a JavaDoc at BirthdayChocolate

--- src/main/java/hackerrank/algorithms/implementations/BirthdayChocolate.java
+++ src/main/java/hackerrank/algorithms/implementations/BirthdayChocolate.java
@@ -1,9 +1,14 @@
 /**
  * Lily has a chocolate bar consisting of a row of  squares where each square has an integer written on it. She wants to share it with Ron for his birthday, which falls on month  and day . Lily only wants to give Ron a piece of chocolate if it contains  consecutive squares whose integers sum to .
-
-Given , , and the sequence of integers written on each square of Lily's chocolate bar, how many different ways can Lily break off a piece of chocolate to give to Ron?
-
-For example, if ,  and the chocolate bar contains  rows of squares with the integers written on them from left to right, the following diagram shows two ways to break off a piece:
+ * 
+ * Given m, d, and the sequence of integers written on each square of Lily's chocolate bar, how many different ways can Lily break off a piece of chocolate to give to Ron?
+ * 
+ * For example, if m=2, d=3  and the chocolate bar contains  rows of squares with the integers written on them from left to right, the following diagram shows two ways to break off a piece:
+ * Input Format
+ * 
+ * The first line contains an integer denoting  (the number of squares in the chocolate bar). 
+ * The second line contains  space-separated integers describing the respective values of s_0, s_1,...,s_(n-1) (the numbers written on each consecutive square of chocolate). 
+ * The third line contains two space-separated integers describing the respective values of  (Ron's birth day) and (Ron's birth month).
  */
 //https://www.hackerrank.com/challenges/the-birthday-bar
 
