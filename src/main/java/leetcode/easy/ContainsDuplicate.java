ea0ceb5de4c8913bc13fc8789836951cdbd51807
Fatos Morina <fatosi.morina@gmail.com>
2017-06-09 23:56:58 +0200

    Put problem source URL above package declaration

--- src/main/java/leetcode/easy/ContainsDuplicate.java
+++ src/main/java/leetcode/easy/ContainsDuplicate.java
@@ -1,9 +1,9 @@
+//https://leetcode.com/problems/contains-duplicate/
 package leetcode.easy;
 
 import java.util.HashSet;
 import java.util.Set;
 
-//https://leetcode.com/problems/contains-duplicate/
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
 	Set<Integer> set = new HashSet<Integer>();
