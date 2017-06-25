1fa8b537f93b1936636b9d989765a74ed864f590
Fatos Morina <fatosi.morina@gmail.com>
2017-06-25 11:55:05 +0200

    Add missing values at DiagonalDifference

--- src/main/java/hackerrank/algorithms/warmup/DiagonalDifference.java
+++ src/main/java/hackerrank/algorithms/warmup/DiagonalDifference.java
@@ -3,10 +3,10 @@
  * 
  * Input Format
  * 
- * The first line contains a single integer, . The next  lines denote the matrix's rows, with each line containing space-separated integers describing the columns.
+ * The first line contains a single integer, N. The next N lines denote the matrix's rows, with each line containing N space-separated integers describing the columns.
  * 
  * Constraints
- * 
+ * -100 <= Elements in the matrix <= 100
  * Output Format
  * 
  * Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
