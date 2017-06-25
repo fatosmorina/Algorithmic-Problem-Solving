a0f4fd43d7f152fdc505231d9b1c0110c6ba8d85
Fatos Morina <fatosi.morina@gmail.com>
2017-06-25 11:53:54 +0200

    Add problem description at DiagonalDifference

--- src/main/java/hackerrank/algorithms/warmup/DiagonalDifference.java
+++ src/main/java/hackerrank/algorithms/warmup/DiagonalDifference.java
@@ -1,3 +1,44 @@
+/**
+ * Given a square matrix of size , calculate the absolute difference between the sums of its diagonals.
+ * 
+ * Input Format
+ * 
+ * The first line contains a single integer, . The next  lines denote the matrix's rows, with each line containing space-separated integers describing the columns.
+ * 
+ * Constraints
+ * 
+ * Output Format
+ * 
+ * Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+ * 
+ * Sample Input
+ * 
+ * 3
+ * 11 2 4
+ * 4 5 6
+ * 10 8 -12
+ * Sample Output
+ * 
+ * 15
+ * Explanation
+ * 
+ * The primary diagonal is:
+ * 
+ * 11
+ *   5
+ *   -12
+ * Sum across the primary diagonal: 11 + 5 - 12 = 4
+ * 
+ * The secondary diagonal is:
+ * 
+ *     4
+ *   5
+ * 10
+ * Sum across the secondary diagonal: 4 + 5 + 10 = 19 
+ * Difference: |4 - 19| = 15
+ * 
+ */
+
 //https://www.hackerrank.com/challenges/diagonal-difference/submissions/code/20132371
 
 package hackerrank.algorithms.warmup;
