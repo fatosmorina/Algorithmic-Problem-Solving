4a0d15fcbb36def4dc4a3af28ca4d417edc86808
Fatos Morina <fatosi.morina@gmail.com>
2017-07-08 18:47:36 +0200

    Add problem descritpion for  FirstUniqueCharacterInString

--- src/main/java/leetcode/easy/FirstUniqueCharacterInString.java
+++ src/main/java/leetcode/easy/FirstUniqueCharacterInString.java
@@ -1,3 +1,16 @@
+/**
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+ * 
+ * Examples:
+ * 
+ * s = "leetcode"
+ * return 0.
+ * 
+ * s = "loveleetcode",
+ * return 2.
+ * Note: You may assume the string contain only lowercase letters.
+ */
+
 // https://leetcode.com/problems/first-unique-character-in-a-string/
 
 package leetcode.easy;
